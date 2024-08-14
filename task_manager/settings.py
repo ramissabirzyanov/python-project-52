3de9a414a05da2711cea662b65a498adc0fbc8db
Ramis <ramis@Ramiss-Air.Dlink>
2024-08-14 16:41:49 +0300

    push to peploy

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -83,11 +83,16 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR/ 'db.sqlite3',
+    }
+}
+
+DATABASES['default'] =  dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
         conn_max_age=600
-    )
-}
+)
 
 
 # Password validation
