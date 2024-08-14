0d0e7d21a4bd96591c2906b3c06f69e3dfa7c9b2
Ramis <ramis@Ramiss-Air.Dlink>
2024-08-14 12:41:35 +0300

    prepare to deploy add gitignore

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://ramis:861110@localhost:5432/task_manager',
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600
     )
 }
