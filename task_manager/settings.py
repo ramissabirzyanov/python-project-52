440b8182e70ed6501f5f2dccf0b7f72d98b2bf0f
Ramis <ramis@Ramiss-Air.Dlink>
2024-08-14 16:04:57 +0300

    try deploy

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'webserver']
 
 
 # Application definition
