2bcd46107be6795d7ea9ef99a32d749ae4d3c529
Ramis <ramis@Ramiss-Air.Dlink>
2024-09-30 15:09:09 +0300

    update

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -101,7 +101,8 @@ DATABASES = {
 DATABASES['default'] = dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
-        conn_health_checks=True
+        conn_health_checks=True, 
+        engine='django.db.backends.postgresql',
 )
 
 ROLLBAR = {
