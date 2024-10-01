2b3ac62aa3c7cbf12e843c06f4d76513265720ec
Ramis <ramis@Ramiss-Air.Dlink>
2024-10-01 17:31:47 +0300

    edit conf #2

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
-import dj_database_url
 import os
+import dj_database_url
 
 
 load_dotenv()
