85b035913c17a8bcdb527a5787c7fdbb838dc593
Ramis <ramis@Ramiss-Air.Dlink>
2024-10-02 18:02:46 +0300

    fix trans create user

--- task_manager/user/forms.py
+++ task_manager/user/forms.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class UserCreateForm(UserCreationForm):
+    def __init__(self, request=None, *args, **kwargs):
+        super().__init__(request=None, *args, **kwargs)
+        self.fields['first_name'].label = _('First name')
+        self.fields['last_name'].label = _('Last name')
+        self.fields['username'].label = _('Username')
+        self.fields['password'].label = _('Password')
+        self.fields['password_confirmation'].label = _('Password confirmation')
+
     usable_password = None
 
     class Meta:
