d9da77450880bb480e79d5e5392163dcf5b3307b
Ramis <ramis@Ramiss-Air.Dlink>
2024-10-02 18:19:33 +0300

    fixing fields in forms

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -5,5 +5,5 @@ from django.utils.translation import gettext_lazy as _
 class CustomAuthenticationForm(AuthenticationForm):
     def __init__(self, request=None, *args, **kwargs):
         super().__init__(request=None, *args, **kwargs)
-        # self.fields['username'].label = _('Username')
-        # self.fields['password'].label = _('Password')
+        self.fields['username'].label = _('Username')
+        self.fields['password'].label = _('Password')
