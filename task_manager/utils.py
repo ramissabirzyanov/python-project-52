094a85c360af39b904362ae51d859dc9e8d60ee5
Ramis <ramis@Ramiss-Air.Dlink>
2024-10-04 14:07:36 +0300

    remove form attr in userloginview

--- task_manager/utils.py
+++ task_manager/utils.py
@@ -4,16 +4,6 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-# class LoginRequiredMixin():
-
-#     def dispatch(self, request, *args, **kwargs):
-#         if not request.user.is_authenticated:
-#             messages.error(request,
-#                            _("You are not logged in! Please log in."))
-#             return redirect('login')
-#         return super().dispatch(request, *args, **kwargs)
-
-
 class CurrentUserCheckMixin(LoginRequiredMixin):
 
     def dispatch(self, request, *args, **kwargs):
--- task_manager/views.py
+++ task_manager/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth import views
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
-from .forms import CustomAuthenticationForm
 from django.utils.translation import gettext as _
 
 
@@ -13,7 +12,6 @@ def index(request):
 class UserLoginView(SuccessMessageMixin, views.LoginView):
     template_name = "login.html"
     success_message = _('You are logged in')
-    authentication_form = CustomAuthenticationForm
 
 
 class UserLogoutView(views.LogoutView):
