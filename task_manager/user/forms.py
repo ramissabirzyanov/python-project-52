1f8e1597e5f0ce29692ef653b4890cedd970abe4
Ramis <ramis@Ramiss-Air.Dlink>
2024-10-05 12:00:39 +0300

    edit po file

--- task_manager/user/forms.py
+++ task_manager/user/forms.py
@@ -1,11 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 from django import forms
 
 
 class UserCreateForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
+    first_name = forms.CharField(max_length=30,
+                                 required=True,
+                                 label=_('First name'))
+    last_name = forms.CharField(max_length=30,
+                                required=True,
+                                label=_('Last name'))
     usable_password = None
 
     class Meta:
